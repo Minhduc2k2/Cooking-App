7dae96f6e92ada6d212db85fe8431391d3b16c7a
Nguyễn Minh Đức <mingduc17082002@gmail.com>
2022-07-24 11:11:37 +0700

    Finish Home and Recipes

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 import "./RecipeList.css";
 function RecipeList({ recipes }) {
-  return recipes.map((recipe) => (
-    <div className="card" key={recipe.id}>
-      <h3>{recipe.title}</h3>
-      <p>{recipe.cookingTime} to make</p>
-      <div>{recipe.method.substring(0, 100)}...</div>
-      <Link to={`/recipe/${recipe.id}`}>Cook this</Link>
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <div className="card" key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to make</p>
+          <div>{recipe.method.substring(0, 100)}...</div>
+          <Link to={`/recipe/${recipe.id}`}>Cook this</Link>
+        </div>
+      ))}
     </div>
-  ));
+  );
 }
 
 export default RecipeList;
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,6 +1,28 @@
 import "./Recipe.css";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 function Recipe() {
-  return <h1>Recipe Page</h1>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="Loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Take {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Recipe;
